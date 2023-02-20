495ecd95294ad16e627363264699f617b724c3fc
Jorge Cobo <cobo-angel@hotmail.com>
2023-02-20 11:21:23 -0400

    Noise visualization structure

---
+++ Assets/Scripts/NoiseVisualization.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class NoiseVisualization : Visualization
+{
+    static int noiseId = Shader.PropertyToID("_Noise");
+
+    [SerializeField] int seed;
+    [SerializeField] SpaceTRS domain = new SpaceTRS {
+        scale = 8f
+    };
+    [SerializeField] Shape shape;
+    [SerializeField, Range(0.1f, 10f)] float instanceScale = 2f;
+    [SerializeField] Mesh instanceMesh;
+    [SerializeField] Material material;
+    [SerializeField, Range(1, 512)]int resolution = 16;
+	[SerializeField, Range(-0.5f, 0.5f)]float displacement = 0.1f;
+
+    
+    NativeArray<float4> noise;
+    ComputeBuffer noiseBuffer;
+
+    protected override void EnableVisualization(int dataLength, MaterialPropertyBlock propertyBlock) 
+    {
+        noise = new NativeArray<float4>(dataLength, Allocator.Persistent);
+        //positions = new NativeArray<float3x4>(length, Allocator.Persistent);
+        //normals = new NativeArray<float3x4>(length, Allocator.Persistent);
+        noiseBuffer = new ComputeBuffer(dataLength * 4, 4);
+        propertyBlock.SetBuffer(noiseId, noiseBuffer);
+    }
+
+    protected override void DisableVisualization() 
+    {
+        noise.Dispose();
+        noiseBuffer.Release();
+        noiseBuffer = null;
+    }
+
+    protected override void UpdateVisualization(NativeArray<float3x4> positions, int resolution, JobHandle handle) 
+    {
+        handle.Complete();
+        noiseBuffer.SetData(noise.Reinterpret<float>(4 * 4));
+    }
+}
